47f8af81e11f94bd555720764f54245290a551da
Huanfeng Cheng <huanfeng@buffalo.edu>
2023-03-27 15:40:12 -0400

    start to implement the endpoints.

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+from db import stores, items
+import uuid
+
+app = Flask(__name__)
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+
+@app.post("/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item
+    return {"message": "Store not found"}, 404
+
+
+@app.get("store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
